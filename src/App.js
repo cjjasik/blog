b68e186d6a566f4a03a9e74a28e2c8193b3396c1
Christina Jasik <cjjasik@gmail.com>
2023-08-24 14:19:21 -0500

    Nav bar works, hero image does not yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+// import { styled } from '@mui/material';
 
 function App() {
+
+  const styles = {
+    myAppBar: {
+      backgroundColor: 'black'
+    },
+    heroBox: {
+      backgroundImage: `liniear-gradient.(rgba(0, 0, 0.5), rgba(0, 0, 0.5)), url('https://images.pexels.com/photos/2190283/pexels-photo-2190283.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')`,
+      height: '500px',
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      positoin:"relative",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "#fff",
+      fontSize: "4rem"
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static" sx={styles.myAppBar}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hungry in Manhattan
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={styles.heroBox}>Foodie Blog</Box>
     </div>
   );
 }
